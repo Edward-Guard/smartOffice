250eda61a47878598c97b3a94b7dc643e842272c
Edward Silva <107967519+Edward-Guard@users.noreply.github.com>
2024-05-31 19:52:03 -0300

    Fix:Alteração de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AplicationProvider from './Context/AplicationProvider';
-import MainScreen from './MainScreen';
+import NavBar from './MainScreen/NavBar';
+import LoginPage from './MainScreen/Pages/Login';
+import StockPage from './MainScreen/Pages/StockPage';
+import ServicesPage from './MainScreen/Pages/ServicesPage';
+import EmployeesPage from './MainScreen/Pages/EmployeesPage';
+import { useEffect, useState } from 'react';
 
 function App() {
+const [path, setPath] = useState('/');
+
+  useEffect(() => {
+    const newPath = window.location.pathname;
+    setPath(newPath);
+  }, []);
+
   return (
     <AplicationProvider>
       <BrowserRouter>
         <div className="principalScreen">
-          <MainScreen />
+          <div className="MainScreen">
+          {(path !== '/') && <NavBar />}
+          <Routes>
+            <Route path="/" element={ <LoginPage /> } />
+            <Route path="/stock" element={ <StockPage /> } />
+            <Route path="/services" element={ <ServicesPage /> } />
+            <Route path="/employees" element={ <EmployeesPage /> } />
+          </Routes>
+          </div>
         </div>
       </BrowserRouter>
     </AplicationProvider>
