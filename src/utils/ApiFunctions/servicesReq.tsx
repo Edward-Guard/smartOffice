5c1e745a3b6bdf7f31a680bc2db2ae6d41d91074
Edward Silva <107967519+Edward-Guard@users.noreply.github.com>
2024-06-03 21:07:15 -0300

    Alter ApiFunctions 2

--- src/utils/ApiFunctions/servicesReq.tsx
+++ src/utils/ApiFunctions/servicesReq.tsx
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
 export const getAllServices = async () => {
-  const response = await axios.get('https://localhost:3001/services');
+  const response = await axios.get('http://localhost:3001/services');
   return response;
 };
 
 export const removeService = async (id: number) => {
-  const response = await axios.delete(`https://localhost:3001/services/${id}`);
+  const response = await axios.delete(`http://localhost:3001/services/${id}`);
   return response;
 };
 
 export const addService = async (data: any) => {
-  const response = await axios.post('https://localhost:3001/services', data);
+  const response = await axios.post('http://localhost:3001/services', data);
   return response;
 };
 
 export const updateService = async (data: any) => {
-  const response = await axios.put(`https://localhost:3001/services/${data.id}`, data);
+  const response = await axios.put(`http://localhost:3001/services/${data.id}`, data);
   return response;
 };
